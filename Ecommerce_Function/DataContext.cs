cdabec560c47e1c40e47661ceadb247e65d3e106
awesomeedl <82195917+awesomeedl@users.noreply.github.com>
2023-02-20 00:08:38 +0800

    Initial commit

---
+++ Ecommerce_Function/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using Ecommerce_Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ecommerce_Function;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    
+    public DbSet<Product> Products { get; set; }
+}
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var constr = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ConnectionString");
+        Debug.Assert(!string.IsNullOrEmpty(constr));
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer(constr);
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
---
+++ Ecommerce_Function/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Azure.Functions.Extensions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Ecommerce_Function.Startup))]
+
+namespace Ecommerce_Function;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddDbContext<DbContext>(
+            options => options.UseSqlServer(Environment.GetEnvironmentVariable(
+                    "SQLAZURECONNSTR_ConnectionString") ?? throw new Exception("Sql connection string not found")));
+    }
+}
